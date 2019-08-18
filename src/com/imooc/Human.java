d8c1c2bd3e7058a773ec039c4c33cee2347095dc
Mr.Liu <848381292@qq.com>
2019-08-18 21:09:04 +0800

    首次新增文件

---
+++ src/com/imooc/Human.java
@@ -0,0 +1,6 @@
+package com.imooc;
+
+public abstract class Human {
+	public abstract void say();
+
+}
---
+++ src/com/imooc/Test.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Human p1=new Chinese();
+        Human p2=new American();
+        p1.say();
+        p2.say();
+	}
+
+}
